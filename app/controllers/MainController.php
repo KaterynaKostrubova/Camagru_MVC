e0d6da90527954a6e04abcbfb6f1bf574bd9e31b
KaterynaKostrubova <katerinakostrubova@gmail.com>
2020-05-06 14:46:17 +0300

    add check

--- app/controllers/MainController.php
+++ app/controllers/MainController.php
@@ -13,19 +13,21 @@ class MainController extends Controller {
 //    }
 
     public function indexAction() {
-
         $model = new Account();
-        $user = $model->getUser($_SESSION['authorize']['name']);
-        $avatarPath = $this->model->getPath($user[0]['photo_id']);
-        $bgPath = $this->model->getPath($user[0]['bg_id']);
+        if($_SESSION && $_SESSION['authorize']['name']) {
+            $user = $model->getUser($_SESSION['authorize']['name']);
+            $avatarPath = $this->model->getPath($user[0]['photo_id']);
+            $bgPath = $this->model->getPath($user[0]['bg_id']);
 
-        $vars = [
-            'info' => $user,
-            'avatar'=> $avatarPath,
-            'bg_photo'=> $bgPath,
-        ];
+            $vars = [
+                'info' => $user,
+                'avatar'=> $avatarPath,
+                'bg_photo'=> $bgPath,
+            ];
 
-        $this->view->render('MAIN PAGE', $vars);
+            $this->view->render('MAIN PAGE', $vars);
+        }
+
     }
 
 }
